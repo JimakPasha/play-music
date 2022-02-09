5302b51aaf6684ff3765612bc6c21378e2b8840b
pavel <pavel@pavel.com>
2022-02-09 14:49:16 +0300

    feat: add next.js typescript material-ui

---
+++ client/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const Index = () => {
+  return (
+    <div>
+        Main page
+        <Button>Click</Button>
+    </div>
+  )
+}
+
+export default Index;
