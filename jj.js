6debd94e433b893729b5f99f9959b98eb599e31d
pavel <pavel@pavel.com>
2022-03-02 18:03:33 +0300

    fix commit

---
+++ jj.js
@@ -0,0 +1,6 @@
+let list1 = [
+	{ firstName: 'Aba', lastName: 'N.', country: 'Ghana', continent: 'Africa', age: 21, language: 'Python' },
+	{ firstName: 'Abb', lastName: 'O.', country: 'Israel', continent: 'Asia', age: 39, language: 'Java' },
+];
+
+console.log(findOddNames(list1));
